e5008d3f91b3c23fde74b286dc1ec97559f44e87
AhmedSamy16 <ahmed.samy1640@gmail.com>
2024-02-26 05:02:55 +0200

    Project Completed

--- App.tsx
+++ App.tsx
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, TextInput, View } from 'react-native';
+import JSONDATA from "./assets/MOCK_DATA.json"
 
 export default function App() {
+  const [searchTerm, setSearchTerm] = useState("")
+
+  const filteredNames = JSONDATA.filter(n => searchTerm === "" ? n : n.first_name.toLowerCase().includes(searchTerm.toLowerCase()))
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <TextInput 
+          value={searchTerm}
+          onChangeText={(text) => setSearchTerm(text)}
+          style={styles.input}
+          placeholder='Search...'
+        />
+        <FlatList
+          data={filteredNames}
+          renderItem={({ item }) => (
+            <View key={item.first_name + item.last_name}>
+              <Text style={styles.text}>{item.first_name}</Text>
+            </View>
+          )}
+          keyExtractor={(item) => item.first_name + item.last_name}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    marginTop: StatusBar.currentHeight
   },
+  input: {
+    marginVertical: 15,
+    marginHorizontal: 0,
+    width: 280,
+    height: 40,
+    paddingLeft: 10,
+    fontSize: 20,
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 3
+  },
+  text: {
+    margin: 10,
+    fontSize: 18,
+    textAlign: 'center'
+  }
 });
---
+++ README.md
@@ -0,0 +1,6 @@
+# 02.Search-filter-project-React_Native
+This is project 2/50 of my React Native projects series.
+
+it's an app that allows users to enter search keys and it filters the user list from the hardcode list based on the entered key.
+
+the file search-filter.gif is a representation of the App
